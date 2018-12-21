77d73183625c3aab7de36a2bde4ea56ca52fa912
wanghx <whx89768@alibaba-inc.com>
2018-12-21 15:52:10 +0800

    feat: update types

--- index.d.ts
+++ index.d.ts
@@ -18,22 +18,40 @@ type ExpectedType = string | RegExp;
  *   assertFile(`${fixtures}/README.md`, [ 'hello', /world/ ]); // support multiple rule
  * ```
  */
-export function ok(file: string, pattern?: ExpectedType | ExpectedType[]): void;
+declare function assertFile(file: string, pattern?: ExpectedType | ExpectedType[]): void;
 
-/**
- * The opposite assertion of assert file
- *
- * @param {String} file - file path
- * @param {String|RegExp|Array} [pattern] - test pattern
- *
- * @example
- * ```js
- *   assertFile.fail(`${fixtures}/package.json`);
- *   assertFile.fail(`${fixtures}/package.json`, { name: 'example' });
- *   assertFile.fail(`${fixtures}/README`, /x = y/);
- *   assertFile.fail(`${fixtures}/README`, [ 'abc', /\d+/ ]);
- * ```
- */
-export function fail(file: string, pattern?: ExpectedType | ExpectedType[]): void;
+declare namespace assertFile {
+  /**
+   * The opposite assertion of assert file
+   *
+   * @param {String} file - file path
+   * @param {String|RegExp|Array} [pattern] - test pattern
+   *
+   * @example
+   * ```js
+   *   assertFile.ok(`${fixtures}/package.json`);
+   *   assertFile.ok(`${fixtures}/package.json`, { name: 'example' });
+   *   assertFile.ok(`${fixtures}/README`, /x = y/);
+   *   assertFile.ok(`${fixtures}/README`, [ 'abc', /\d+/ ]);
+   * ```
+   */
+  export function ok(file: string, pattern?: ExpectedType | ExpectedType[]): void;
 
-export default ok;
+  /**
+   * The opposite assertion of assert file
+   *
+   * @param {String} file - file path
+   * @param {String|RegExp|Array} [pattern] - test pattern
+   *
+   * @example
+   * ```js
+   *   assertFile.fail(`${fixtures}/package.json`);
+   *   assertFile.fail(`${fixtures}/package.json`, { name: 'example' });
+   *   assertFile.fail(`${fixtures}/README`, /x = y/);
+   *   assertFile.fail(`${fixtures}/README`, [ 'abc', /\d+/ ]);
+   * ```
+   */
+  export function fail(file: string, pattern?: ExpectedType | ExpectedType[]): void;
+} 
+
+export = assertFile;
